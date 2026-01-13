b715b8569703d07f0cdc2b05ffb960fe91349d4f
tuannguyen20198 <lntt20198@gmail.com>
2026-01-13 21:51:58 +0700

    feat(auth): handle ssocallbackpage

--- apps/web/app/(auth)/sign-in/sso-callback/page.tsx
+++ apps/web/app/(auth)/sign-in/sso-callback/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { useAuth } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 
 export default function SsoCallbackPage() {
-    const { isLoaded, isSignedIn } = useAuth();
-    const router = useRouter();
-
-    useEffect(() => {
-        if (!isLoaded) return;
-
-        if (isSignedIn) {
-            router.replace("/dashboard");
-        }
-    }, [isLoaded, isSignedIn, router]);
-
-    return <div>Verifying your session…</div>;
+    return <AuthenticateWithRedirectCallback />;
 }
